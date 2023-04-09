925a975c118df6e42a13f2a415e0b592e1be60fe
cooderyg <ygk9604@naver.com>
2023-04-10 08:37:49 +0900

    banner ㅕㅑ

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios'
 import requests from '../api/request'
-
+import "./Banner.css"
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
+    fetchData();
   }, [])
   const fetchData = async () => {
     //현재 상영중 영화정보 가져오기(20개)
@@ -20,10 +21,37 @@ const Banner = () => {
     })
     setMovie(movieDetail)
   }
-  fetchData();
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  }
   return (
     <div>
-      
+      <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+      >
+        <div className="banner__contnets">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className="banner__button">
+            {movie?.videos?.results[0]?.key &&
+              <button>
+                Play
+              </button>
+            }
+
+          </div>
+          <p className="banner__description">
+            {truncate(movie.overview, 100)}
+          </p>
+        </div>
+            <div className="banner--fadeBottom"></div>
+      </header>
     </div>
   )
 }
